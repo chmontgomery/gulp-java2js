f3ce4af36f9a3b805235fb23746012208dc4ab72
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-02-26 13:14:31 -0600

    This is for your own safety.

--- README.md
+++
@@ -1,2 +0,0 @@
-# gulp-java2js
-convert java classes to javascript via a gulp task. Hooray!
--- index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function () {
-	console.log("The worst/best idea you've ever had");
-	// TODO: implement remaining logic
-}
