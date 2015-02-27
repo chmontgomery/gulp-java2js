27a268d942053bf7e902eee19f664e99bf939d43
Rob Zienert <rob@robzienert.com>
2015-02-27 11:18:46 -0600

    Implementing conversion logic

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 module.exports = function () {
 	console.log("The best worst idea you've ever had");
-	// TODO: implement remaining logic
-}
+
+	var gulp = require('gulp'),
+		es = require('event-stream'),
+		log = require('consolelogger');
+
+	var printFileNames = function() {
+		return es.map(function(data, cb) {
+			cb(null, data);
+		});
+	};
+
+	var forEachFolder = function(stream) {
+		return es.map(function(data, cb) {
+			if (data.isDirectory()) {
+				var path = data.path + '/*.java',
+					jsPath = path.substr(0, file.lastIndexOf('.')) + '.js';
+				log.info(['Converting', path, 'to javascript'].join(' '));
+				if (stream !== undefined) {
+					gulp.src(path)
+						.pipe(gulp.dest(jsPath));
+				}
+			}
+		});
+	};
+
+	gulp.task('j2js', function() {
+		gulp.src('src/*')
+			.pipe(forEachFolder(printFilenames));
+	});
+};
